29e8f7fdc32d03372e80ab8552f85904e3a175d7
tdgog <tdgog321@gmail.com>
2023-11-04 00:20:59 +0000

    Cleaned up ordering of the SyntaxKind enum

--- src/main/java/io/github/tdgog/compiler/Syntax/SyntaxKind.java
+++ src/main/java/io/github/tdgog/compiler/Syntax/SyntaxKind.java
@@ -4,19 +4,22 @@ package io.github.tdgog.compiler.Syntax;
  * The types of tokens and expressions in the language.
  */
 public enum SyntaxKind {
+    // Tokens
+    BadToken,
+    EOFToken,
+    WhitespaceToken,
     IntegerToken,
     FloatToken,
     PlusToken,
     MinusToken,
     MultiplyToken,
     DivideToken,
+    ModuloToken,
     OpenBracketToken,
     CloseBracketToken,
-    BadToken,
-    EOFToken,
+
+    // Expressions
     BinaryExpression,
     BracketExpression,
-    ModuloToken,
-    LiteralExpression,
-    WhitespaceToken
+    LiteralExpression
 }
