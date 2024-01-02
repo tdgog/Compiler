567f42519bd61156a43476ea9908a1a5f19ea584
tdgog <65415295+tdgog@users.noreply.github.com>
2024-01-02 01:36:32 +0000

    Break down the evaluator multiple methods for evaluating different expression types

--- src/main/java/io/github/tdgog/compiler/Evaluation/Evaluator.java
+++ src/main/java/io/github/tdgog/compiler/Evaluation/Evaluator.java
@@ -16,7 +16,6 @@ import lombok.AllArgsConstructor;
 import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
 import java.util.Set;
 
 /**
@@ -34,63 +33,78 @@ public final class Evaluator {
 
     /**
      * Evaluates an expression
-     * @param root The parent node of the expression
+     * @param node The parent node of the expression
      * @return The resulting value
      */
-    private Object evaluateExpression(BoundExpression root) {
-        if (root instanceof BoundLiteralExpression expression)
-            return expression.getValue();
-        else if (root instanceof BoundVariableExpression expression)
-            return variables.get(expression.getVariable());
-        else if (root instanceof BoundAssignmentExpression expression) {
-            Object value = evaluateExpression(expression.getExpression());
-            variables.put(new VariableSymbol(expression.getName(), expression.getType()), value);
-            return value;
-        }
-        else if (root instanceof BoundUnaryExpression expression) {
-            Object operand = evaluateExpression(expression.getOperand());
-            BoundUnaryOperatorKind syntaxKind = expression.getOperator().getOperatorKind();
+    private Object evaluateExpression(BoundExpression node) {
+        return switch (node.getBoundNodeKind()) {
+            case LiteralExpression -> evaluateLiteralExpression((BoundLiteralExpression) node);
+            case VariableExpression -> evaluateVariableExpression((BoundVariableExpression) node);
+            case AssignmentExpression -> evaluateAssignmentExpression((BoundAssignmentExpression) node);
+            case UnaryExpression -> evaluateUnaryExpression((BoundUnaryExpression) node);
+            case BinaryExpression -> evaluateBinaryExpression((BoundBinaryExpression) node);
+            default -> throw new RuntimeException("Unexpected node " + node.getBoundNodeKind());
+        };
+    }
 
-            if (syntaxKind == BoundUnaryOperatorKind.Identity)
-                return operand;
-            else if (syntaxKind == BoundUnaryOperatorKind.Negation) {
-                if (operand instanceof Double o)
-                    return -o;
-                if (operand instanceof Integer o)
-                    return -o;
-            } else if (syntaxKind == BoundUnaryOperatorKind.LogicalNegation) {
-                return !(boolean) operand;
-            }
+    private Object evaluateBinaryExpression(BoundBinaryExpression expression) {
+        Object leftExpression = evaluateExpression(expression.getLeft());
+        Object rightExpression = evaluateExpression(expression.getRight());
+        BoundBinaryOperatorKind operatorKind = expression.getOperator().getOperatorKind();
 
-            throw new RuntimeException("Unexpected unary operator " + syntaxKind);
-        }
-        else if (root instanceof BoundBinaryExpression expression) {
-            Object leftExpression = evaluateExpression(expression.getLeft());
-            Object rightExpression = evaluateExpression(expression.getRight());
-            BoundBinaryOperatorKind operatorKind = expression.getOperator().getOperatorKind();
-
-            if (operatorKind == BoundBinaryOperatorKind.Equals) {
-                return leftExpression.equals(rightExpression);
-            } else if (operatorKind == BoundBinaryOperatorKind.NotEquals) {
-                return !leftExpression.equals(rightExpression);
-            }
-
-            try {
-                Set<Class<? extends Visitor>> visitors = new Reflections("io.github.tdgog.compiler.Evaluation.Visitors").getSubTypesOf(Visitor.class);
-                for (Class<? extends Visitor> clazz : visitors) {
-                    Visitor visitor = clazz.getDeclaredConstructor().newInstance();
-                    if (!visitor.acceptsOperator(operatorKind))
-                        continue;
-
-                    return visitor.visit(leftExpression, rightExpression);
-                }
-            } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
-                     InvocationTargetException | UnexpectedBinaryOperatorException e) {
-                throw new RuntimeException(e);
-            }
+        if (operatorKind == BoundBinaryOperatorKind.Equals) {
+            return leftExpression.equals(rightExpression);
+        } else if (operatorKind == BoundBinaryOperatorKind.NotEquals) {
+            return !leftExpression.equals(rightExpression);
         }
 
-        throw new RuntimeException("Unexpected node " + root.getBoundNodeKind());
+        try {
+            Set<Class<? extends Visitor>> visitors = new Reflections("io.github.tdgog.compiler.Evaluation.Visitors").getSubTypesOf(Visitor.class);
+            for (Class<? extends Visitor> clazz : visitors) {
+                Visitor visitor = clazz.getDeclaredConstructor().newInstance();
+                if (!visitor.acceptsOperator(operatorKind))
+                    continue;
+
+                return visitor.visit(leftExpression, rightExpression);
+            }
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
+                 InvocationTargetException | UnexpectedBinaryOperatorException e) {
+            throw new RuntimeException(e);
+        }
+
+        throw new RuntimeException("Unexpected binary operator " + operatorKind);
+    }
+
+    private Object evaluateUnaryExpression(BoundUnaryExpression expression) {
+        Object operand = evaluateExpression(expression.getOperand());
+        BoundUnaryOperatorKind syntaxKind = expression.getOperator().getOperatorKind();
+
+        if (syntaxKind == BoundUnaryOperatorKind.Identity)
+            return operand;
+        else if (syntaxKind == BoundUnaryOperatorKind.Negation) {
+            if (operand instanceof Double o)
+                return -o;
+            if (operand instanceof Integer o)
+                return -o;
+        } else if (syntaxKind == BoundUnaryOperatorKind.LogicalNegation) {
+            return !(boolean) operand;
+        }
+
+        throw new RuntimeException("Unexpected unary operator " + syntaxKind);
+    }
+
+    private Object evaluateAssignmentExpression(BoundAssignmentExpression expression) {
+        Object value = evaluateExpression(expression.getExpression());
+        variables.put(new VariableSymbol(expression.getName(), expression.getType()), value);
+        return value;
+    }
+
+    private Object evaluateVariableExpression(BoundVariableExpression expression) {
+        return variables.get(expression.getVariable());
+    }
+
+    private Object evaluateLiteralExpression(BoundLiteralExpression expression) {
+        return expression.getValue();
     }
 
 }
